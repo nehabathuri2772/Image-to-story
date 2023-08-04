dd1772946a10e39d9d2b4d99d524a9c6628b78c1
Sylvain Filoni <fffiloni@users.noreply.huggingface.co>
2023-08-04 16:44:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,27 @@
 import gradio as gr
-import torch
-
-from PIL import Image
-from transformers import InstructBlipProcessor, InstructBlipForConditionalGeneration
-
-model = InstructBlipForConditionalGeneration.from_pretrained("Salesforce/instructblip-vicuna-7b")
-processor = InstructBlipProcessor.from_pretrained("Salesforce/instructblip-vicuna-7b")
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
 
 import os 
 hf_token = os.environ.get('HF_TOKEN')
 from gradio_client import Client
 client = Client("https://fffiloni-test-llama-api.hf.space/", hf_token=hf_token)
 
+clipi_client = Client("https://fffiloni-clip-interrogator-2.hf.space/")
+
+
 def infer(image_input):
-    #img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
-    raw_image = Image.open(image_input).convert('RGB')
-
-    prompt = "Can you please describe what's happening in the image, and give information about the characters and the place ?"
-    inputs = processor(images=raw_image, text=prompt, return_tensors="pt").to(device)
     
-    outputs = model.generate(
-        **inputs,
-        do_sample=False,
-        num_beams=5,
-        max_length=256,
-        min_length=1,
-        top_p=0.9,
-        repetition_penalty=1.5,
-        length_penalty=1.0,
-        temperature=1,
+    clipi_result = clipi_client.predict(
+    				image_input,	# str (filepath or URL to image) in 'parameter_3' Image component
+    				"best",	# str in 'Select mode' Radio component
+    				6,	# int | float (numeric value between 2 and 24) in 'best mode max flavors' Slider component
+    				api_name="/clipi2"
     )
-    generated_text = processor.batch_decode(outputs, skip_special_tokens=True)[0].strip()
-    print(generated_text)
-
+    print(clipi_result)
    
 
     llama_q = f"""
     I'll give you a simple image caption, from i want you to provide a story that would fit well with the image:
-    '{generated_text}'
+    '{clipi_result}'
     
     """
     
@@ -49,12 +30,9 @@ def infer(image_input):
     				api_name="/predict"
     )
 
-    
-    
-    
     print(f"Llama2 result: {result}")
 
-    return generated_text, result
+    return clipi_result, result
 
 css="""
 #col-container {max-width: 910px; margin-left: auto; margin-right: auto;}
@@ -67,9 +45,6 @@ with gr.Blocks(css=css) as demo:
             """
             # Image to Story
             Upload an image, get a story ! 
-            <br/>
-            <br/>
-            [![Duplicate this Space](https://huggingface.co/datasets/huggingface/badges/raw/main/duplicate-this-space-sm.svg)](https://huggingface.co/spaces/fffiloni/SplitTrack2MusicGen?duplicate=true) for longer audio, more control and no queue.</p>
             """
         )
         image_in = gr.Image(label="Image input", type="filepath")
